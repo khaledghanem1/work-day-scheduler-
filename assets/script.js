8900534275778dcc6fdc047f6c4dc7fb09ccc18f
Khaled Ghanem <khaledghanim96@gmail.com>
2022-07-04 17:37:30 -0600

    Changes add to css

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-console.log("hi sakina")
+$(document).ready(function () {
+
+   var currentDate = moment().format("dddd, MMMM Do YYYY");
+   var currentHour = moment().hour();
+   var previousHour = currentHour -1;
+   var htmlTime = $(".hourBlock .time").html()
+    $("#currentDay").text(currentDate)
+     
+    function hourTracking() {
+        if(currentHour === htmlTime) {
+            console.log(currentHour)
+        } else if (currentHour > htmlTime) {
+            console.log(currentHour, previousHour + "previous")
+        }
+    }
+    hourTracking();
+
+
+
+
+});
